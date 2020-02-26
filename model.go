2439b828c6ebbefde6eea222dcb14a25b02351cc
Josh Jacobs <josh@joshjacobs.net>
2020-02-27 07:58:25 +1000

    Model Skeleton Created

---
+++ model.go
@@ -0,0 +1,31 @@
+import (
+	"database/sql"
+	"errors"
+)
+
+type service struct {
+	ID    int     json:"id"
+	Name  string  json:"name"
+	Endpoint float64 json:"endpoint"
+	Command string json:"command"
+}
+
+func (p *service) getService(db *sql.DB) error {
+	return errors.New("Not implemented")
+  }
+  
+  func (p *service) updateService(db *sql.DB) error {
+	return errors.New("Not implemented")
+  }
+  
+  func (p *service) deleteService(db *sql.DB) error {
+	return errors.New("Not implemented")
+  }
+  
+  func (p *service) createService(db *sql.DB) error {
+	return errors.New("Not implemented")
+  }
+  
+  func getServices(db *sql.DB, start, count int) ([]service, error) {
+	return nil, errors.New("Not implemented")
+  }
