d8356fbcc939a9e14b7cfb3a386f01dea0462b4e
Josh Jacobs <josh@joshjacobs.net>
2020-02-29 12:07:04 +1000

    Correct Logger

--- logger.go
+++ logger.go
@@ -7,12 +7,12 @@ import (
 
 //Logger - logs things
 func Logger(msg string) {
-	f, err := os.OpenFile("sessions.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile("fink.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
 	}
 	defer f.Close()
 
-	logger := log.New(f, "MPGS_API", log.LstdFlags)
+	logger := log.New(f, "FINK", log.LstdFlags)
 	logger.Println(msg)
 }
